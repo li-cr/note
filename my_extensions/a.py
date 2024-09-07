16f0d0853437cf523e332e5cc6113d3ade889629
lcr <lcr@note.mail>
2024-09-08 00:41:49 +0800

    obsidian note : 2024-09-08 00:41:49
    Affected files:
    mkdocs.yml
    my_extensions/a.py

--- my_extensions/a.py
+++ my_extensions/a.py
@@ -1,22 +1,6 @@
-# my_extensions/code_block_extension.py
-from markdown.extensions import Extension
-from markdown.inlinepatterns import InlineProcessor
-import re
+from mkdocs.plugins import BasePlugin
 
 
-class CodeBlockExtension(Extension):
-    def extendMarkdown(self, md):
-        md.inlinePatterns.register(CodeBlockPattern(), "code_block", 175)
-
-
-class CodeBlockPattern(InlineProcessor):
-    def handleMatch(self, m, data):
-        # 实现你的解析逻辑，例如提取 file:"dawd"
-        code_block_content = m.group(0)
-        # 解析逻辑，例如提取 file:"dawd" 信息
-        # 返回处理后的内容
-        return "", m.start(), m.end()
-
-
-def makeExtension(**kwargs):
-    return CodeBlockExtension(**kwargs)
+class MyMkdocsPlugin(BasePlugin):
+    def on_pag_content(self, content, page, config):
+        return content
